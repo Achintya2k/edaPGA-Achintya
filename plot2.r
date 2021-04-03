9205b03fca6113bb51954d85de663abed5298baa
Achintya RAstogi <45621749+Achintya2k@users.noreply.github.com>
2021-04-03 15:29:22 +0530

    plot 2 commit

---
+++ plot2.r
@@ -0,0 +1,13 @@
+Dataset <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?")
+data <- subset(Dataset, Date %in% c("1/2/2007","2/2/2007"))
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+## Plot 2
+with(data, {
+  plot(Global_active_power~Datetime, type="l",
+       ylab="Global Active Power (kilowatts)", xlab="")
+})
+
+dev.copy(png, file="plot2.png", height=480, width=480)
